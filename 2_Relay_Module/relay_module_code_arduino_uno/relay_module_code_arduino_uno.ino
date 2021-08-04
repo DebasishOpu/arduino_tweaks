70c0ca1ad45f2830f647215be9efa66534b2caa7
Debasish Bal <debasishopu@gmail.com>
2021-08-04 12:34:07 +0600

    Update relay_module_code_arduino_uno.ino

--- 2_Relay_Module/relay_module_code_arduino_uno/relay_module_code_arduino_uno.ino
+++ 2_Relay_Module/relay_module_code_arduino_uno/relay_module_code_arduino_uno.ino
@@ -1,72 +1,45 @@
 /*
 
-1. Connect the output pin of sensor with A0 pin of Arduino Uno
-2. Provide 5V power supply. 
+1. Connect the input pins of relay module (in this case 4 channel) with 3, 4, 5, and 6 digital pins of Arduino Uno
+2. Provide 5V power supply in the Vin of Relay Module. 
 
 */
 
+const int relay_1_pin = 3;
+const int relay_2_pin = 4;
+const int relay_3_pin = 5;
+const int relay_4_pin = 6;
 
-const int current_sensor_1 = A0;
 
 
 void setup() {
-  Serial.begin(9600); 
-  pinMode(current_sensor_1, INPUT); 
+
+  pinMode(relay_1_pin, OUTPUT);
+  pinMode(relay_2_pin, OUTPUT);
+  pinMode(relay_3_pin, OUTPUT);
+  pinMode(relay_4_pin, OUTPUT);
+
 
 }
 
 void loop() {
+  
+  digitalWrite(relay_1_pin, HIGH);
+  digitalWrite(relay_2_pin, HIGH);
+  digitalWrite(relay_3_pin, HIGH);
+  digitalWrite(relay_4_pin, HIGH);
+  delay(1000);
 
-  current_read(current_sensor_1);
-  Serial.println("______________________________________________");
+  digitalWrite(relay_1_pin, LOW);
+  delay(1000);
 
-}
+  digitalWrite(relay_2_pin, LOW);
+  delay(1000);
 
+  digitalWrite(relay_3_pin, LOW);
+  delay(1000);
 
-void current_read(int sensor_pin) {
-  double current_reading = 0.00;
-  float result = 0.0;
-  float meanResult = 0.0;
-  double VRMS = 0;
-
-
-  int readValue = 0;
-  int maxValue = 0;
-  int minValue = 1024;
-
-  for (int i = 0; i < 1; i++) { //taking single time for elimininting initial beep
-    uint32_t start_time = millis();
-    //sample for 1 Sec
-    while ((millis() - start_time) < 1000) {
-      readValue = analogRead(sensor_pin);
-      if (readValue > maxValue) {
-        maxValue = readValue;
-      }
-      if (readValue < minValue) {
-        minValue = readValue;
-      }
-    }
-
-    result = ((maxValue - minValue) * 5.0) / 1024.0; //Peak to Peak
-
-    meanResult += result;
-
-  }
-
-  result = meanResult / 1;
-
-
-  VRMS = (result / 2.0) * 0.707;
-  current_reading = ((VRMS * 1000) / 66) - 2.84   ; //Ratiometric value for 30A Model
-
-  if (current_reading <= 0) {
-    current_reading = 0;
-  }
-
-
-  Serial.print("current_reading: ");
-  Serial.print(current_reading);
-  Serial.println(" A");
-
+  digitalWrite(relay_4_pin, LOW);
+  delay(1000);
 
 }
